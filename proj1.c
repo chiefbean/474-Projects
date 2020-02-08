a950cd862a11bd1bf03d4ed76095da33f0f73f1f
Steven Karschnia <steven.karschnia@ndsu.edu>
2020-02-08 14:48:04 -0600

    proj1

---
+++ proj1.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+  int fd1[2];
+  int fd2[2];
+  int pid;
+  int n = 1;
+  char *size = '1000';
+
+  if (pipe(fd1)==-1)
+  {
+      fprintf(stderr, "Pipe Failed" );
+      return 1;
+  }
+  if (pipe(fd2)==-1)
+  {
+      fprintf(stderr, "Pipe Failed" );
+      return 1;
+  }
+
+  pid = fork();
+
+  // for(int i = 0; i < n-1; i++) {
+  //   if (pid > 0) {
+  //     pid = fork();
+  //   }
+  // }
+  if (pid > 0) {
+    char *returnValue;
+    float hyTan;
+    close(fd1[0]);
+
+    write(fd1[1], size, 4);
+    close(fd1[1]);
+
+    wait(NULL);
+
+    close(fd2[1]);
+
+    read(fd2[0], returnValue, 6);
+
+    sscanf(returnValue, "%6f", &hyTan);
+
+    printf("Hyperbolic Tan  %6f\n", hyTan);
+    close(fd2[0]);
+  }
+
+  if(pid == 0) {
+    close(fd1[1]);
+    int m;
+    read(fd1[0], m, 4);
+
+    double x;
+    double sum = 0;
+    char *output;
+
+    for(int i = 0; i < m; i++) {
+      x = (float)i / (float)m;
+      sum += tanh(x);
+    }
+
+    sprintf(output, "%6f", sum);
+
+    close(fd1[0]);
+    close(fd2[0]);
+
+    write(fd2[1], output, 6);
+    close(fd2[1]);
+
+    exit(0);
+  }
+}
