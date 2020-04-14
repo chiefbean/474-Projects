de6ef7a0a137d31b8aae4e2303ccd6b053dae3ff
Steven Karschnia <steven.karschnia@ndsu.edu>
2020-04-14 12:44:14 -0500

    .

--- proj2.c
+++ proj2.c
@@ -1,21 +1,64 @@
+// ***********************************************
+// *                                             *
+// *            CSCI 474 Project 2               *
+// * Steven Karschnia, Keon Woo Kim, Ryan Peters *
+// *                                             *
+// ***********************************************
 #include <stdio.h>
 
-void fifo(FILE fp)
+void fifo(char *fileName, int frames, int pages)
 {
+    int page_faults = 0, read;
+    FILE *fp = fopen(fileName, "r");
+    char buff[255];
 
+    int temp[frames];
+    for(int i = 0; i < frames; i++)
+    {
+        temp[i] = -1;
+    }
+    for(int i = 0; i < pages; i++)
+    {
+        int s = 0;
+        fscanf(fp, "%s", buff);
+        read = atoi(buff);
+        for(int j = 0; j < frames; j++)
+        {
+            if(read == temp[j])
+            {
+                s++;
+                page_faults--;
+            }
+        }
+        page_faults++;
+        if((page_faults <= frames) && (s == 0))
+        {
+            temp[i] = read;
+        }
+        else if(s == 0)
+        {
+            temp[(page_faults - 1) % frames] = read;
+        }
+        printf("\n");
+        for(int j = 0; j < frames; j++)
+        {
+            printf("%d\t", temp[j]);
+        }
+    }
+    printf("\ntotal page faults: %d\n", page_faults);
+    fclose(fp);
 }
 
 int main(int argc, char** argv)
 {
     FILE *fp;
-    char buff[255];
 
+    //read in command line arguments
     char *fileName = argv[1];
-    int minFrames = argv[2];
-    int maxFrames = argv[3];
-    int pages = argv[4];
+    int minFrames = atoi(argv[2]);
+    int maxFrames = atoi(argv[3]);
+    int pages = atoi(argv[4]);
 
-    fp = fopen(fileName, "r");
-    fifo(fp);
-    fclose(fp);
+    printf("FIFO \n");
+    fifo(fileName, maxFrames, pages);
 }
